5543dab582ed401dee9378aa9a2aa61f4025bb12
chenq7an <qianch714@gmail.com>
2021-12-13 14:34:43 +0800

    fixed: megacli way add other errors on medium error count

--- common/block/megacli.go
+++ common/block/megacli.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 
 	"github.com/chenq7an/gstor/common/controller"
+	"github.com/spf13/cast"
 )
 
 type megacliCollector struct{}
@@ -27,9 +28,9 @@ func megacli(id string, results chan<- Disk, wg *sync.WaitGroup) {
 	eid := strings.Split(id, ":")[1]
 	sid := strings.Split(id, ":")[2]
 
-	disk := Disk{CES: id}
+	disk := Disk{CES: id, MediaError: "0", PredictError: "0"}
 	// 从阵列卡 Pdinfo 中抓取的信息
-	megacliInfo := Bash(fmt.Sprintf(`%s -Pdinfo -PhysDrv[%s:%s] -a%s | egrep "Device Id:|WWN:|Firmware state:|Media Type:|Media Error Count:|Predictive Failure Count:"`, tool, eid, sid, cid))
+	megacliInfo := Bash(fmt.Sprintf(`%s -Pdinfo -PhysDrv[%s:%s] -a%s | egrep "Device Id:|WWN:|Firmware state:|Media Type:|Media Error Count:|Other Error Count:|Predictive Failure Count:"`, tool, eid, sid, cid))
 
 	pdInfo := strings.Split(strings.Trim(megacliInfo, "\n"), "\n")
 
@@ -49,7 +50,11 @@ func megacli(id string, results chan<- Disk, wg *sync.WaitGroup) {
 			}
 			disk.MediaType = mediaType
 		case strings.Contains(v, "Media Error Count"):
-			disk.MediaError = strings.Trim(strings.Split(v, ":")[1], " ")
+			merr := strings.Trim(strings.Split(v, ":")[1], " ")
+			disk.MediaError = cast.ToString(cast.ToInt(merr) + cast.ToInt(disk.MediaError))
+		case strings.Contains(v, "Other Error Count"):
+			oerr := strings.Trim(strings.Split(v, ":")[1], " ")
+			disk.MediaError = cast.ToString(cast.ToInt(oerr) + cast.ToInt(disk.MediaError))
 		case strings.Contains(v, "Predictive Failure Count"):
 			disk.PredictError = strings.Trim(strings.Split(v, ":")[1], " ")
 		}
